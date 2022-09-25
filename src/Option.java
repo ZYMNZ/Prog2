b642cbb291724a58794876bcb8093ec379937cd9
ZYMNZ <98342638+ZYMNZ@users.noreply.github.com>
2022-09-25 07:23:23 -0400

    Prog2 projects

--- src/Option.java
+++ src/Option.java
@@ -28,19 +28,19 @@ public class Option {
         Scanner sc = new Scanner(System.in);
         boolean flag = true;
         int count = 0;
-        System.out.println("Do you want to enter a num? (1/0)");
-        int choice = sc.nextInt();
-        int arr[] = new int[3];
-        while(flag){
 
+
+        int arr[] = new int[3];
+        while(count<3 && flag){
+            System.out.println("Do you want to enter a num? (1/0)");
+            int choice = sc.nextInt();
             if(choice == 0){
                 flag = false;
             }
             else {
                 System.out.println("Enter a num: ");
-                int option = sc.nextInt();
-                //////////////
-                count++;
+                arr[count++] = sc.nextInt();
+
             }
         }
         switch (count) {
@@ -51,25 +51,14 @@ public class Option {
                 break;
             case 2:
                 if(count == 2){
-                    System.out.println("Power: " + obj.Option(arr[0], arr[1]));
+                    System.out.println("Power is: " + obj.Option(arr[0], arr[1]));
                 }
-//                System.out.println("Enter a number: ");
-//                int base = sc.nextInt();
-//                System.out.println("Enter a power: ");
-//                int power = sc.nextInt();
-//                System.out.println("The power of " + base + " is: " + obj.Option(base, power));
                 break;
             case 3:
                 if(count == 3){
                     System.out.println("Sum: " + obj.Option(arr[0], arr[1], arr[2]));
                 }
-//                System.out.println("Enter a number: ");
-//                int sun1 = sc.nextInt();
-//                System.out.println("Enter a number: ");
-//                int sum2 = sc.nextInt();
-//                System.out.println("Enter a number: ");
-//                int sum3 = sc.nextInt();
-//                System.out.println("The sum of " + sun1 + " and " + sum2 + " and " + sum3 + " is: " + obj.Option(sun1, sum2, sum3));
+
                 break;
             default:
                 System.out.println("Invalid option");
